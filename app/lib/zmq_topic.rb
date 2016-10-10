aff41a081a10ab9665a47bc71451d1970504458a
Omri Schwarz <oschwarz@cse.fraunhofer.org>
2016-10-10 17:40:26 -0400

    Get its own mutex

--- app/lib/zmq_topic.rb
+++ app/lib/zmq_topic.rb
@@ -3,19 +3,22 @@
 
 #require 'singleton'
 require 'jrzmq'
-#require 'app/lib/oadr_logger'
+require 'app/lib/oadr_logger'
 
 class ZmqTopic
-  #include Singleton
+  include Singleton
   # todo: is this necessary?
   def initialize
     OadrLogger.instance.log_info('starting ZMQ service')
-
+    @mutex  = Mutex.new
     @context = ZMQ::Context.new(1)
     @outbound = @context.socket(ZMQ::PUB )
     @outbound.bind("tcp://*:9099")
   end
   def post (msg)
-    @outbound.send_string(msg)
+    with_mutex {@outbound.send_string(msg)}
+  end
+  def with_mutex
+    @mutex.synchronize { yield }
   end
 end
