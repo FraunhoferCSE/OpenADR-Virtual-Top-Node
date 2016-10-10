8ace64eac9e7635c4c0871432d581f0344caf908
Omri Schwarz <oschwarz@cse.fraunhofer.org>
2016-10-10 16:38:46 -0400

    logging

--- app/lib/zmq_topic.rb
+++ app/lib/zmq_topic.rb
@@ -3,11 +3,13 @@
 
 require 'singleton'
 require 'jrzmq'
+require 'app/lib/oadr_logger'
 
 class ZmqTopic
   include Singleton
-
+  # todo: is this necessary?
   def initialize
+    OadrLogger.instance.log_info('starting ZMQ service')
 
     @context = ZMQ::Context.new(1)
     @outbound = @context.socket(ZMQ::PUB )
